8cd545397de7d2518fb036548bc10ee78d6b77b6
sqlasskicker <63858650+sqlasskicker@users.noreply.github.com>
2020-09-02 01:57:56 +0530

    Add file

---
+++ termux_pkg.py
@@ -0,0 +1,159 @@
+import os
+os.system('clear')
+
+#figlet & toilet
+os.system('echo **************installing**********')
+print('\n_______________________________________________________\n')
+os.system('apt install figlet && apt install toilet')
+print('\n_______________________________________________________\n')
+os.system('clear')
+
+
+
+#Ruby
+os.system('toilet --filter border  RUBY')
+os.system('echo *************installing***********')
+print('\n_______________________________________________________\n')
+os.system('pkg install ruby')
+print('\n_______________________________________________________\n')
+os.system('figlet DONE')
+print('\n_______________________________________________________\n')
+
+#Gem
+os.system('toilet --filter border  GEM')
+os.system('echo *************installing***********')
+print('\n________________________________________________________\n')
+os.system('gem install lolcat')
+print('\n________________________________________________________\n')
+os.system('figlet DONE')
+print('\n________________________________________________________\n')
+
+
+
+#git
+os.system('toilet --filter border  git|lolcat')
+os.system('echo ***************installing*********|lolcat')
+print('\n_______________________________________________________\n')
+os.system('pkg install git')
+print('\n_______________________________________________________\n')
+os.system('figlet DONE|lolcat')
+print('\n_______________________________________________________\n')
+
+
+
+#Python
+os.system('toilet --filter border  PYTH0N|lolcat')
+os.system('echo **************installing************|lolcat')
+print('\n_______________________________________________________\n')
+os.system('apt install python -y')
+print('\n_______________________________________________________\n')
+os.system('figlet DONE|lolcat')
+print('\n_______________________________________________________\n')
+
+
+#Python2
+os.system('toilet --filter border  PYTHON2|lolcat')
+os.system('echo *********installing********|lolcat')
+print('\n_______________________________________________________\n')
+os.system('apt install python2')
+print('\n_______________________________________________________\n')
+os.system('figlet DONE|lolcat')
+print('\n_______________________________________________________\n')
+
+#pip
+os.system('toilet --filter border  PIP UPGRADE|lolcat')
+os.system('echo *************upgradign***********|lolcat')
+print('\n_______________________________________________________\n')
+os.system('pip install --upgrade pip')
+print('\n_______________________________________________________\n')
+os.system('figlet DONE|lolcat')
+print('\n_______________________________________________________\n')
+
+
+#colorama
+os.system('toilet --filter border  COLORAMA|lolcat')
+os.system('echo **************installing*************|lolcat')
+print('\n_______________________________________________________\n')
+os.system('pip install colorama')
+print('\n_______________________________________________________\n')
+os.system('figlet DONE|lolcat')
+print('\n_______________________________________________________\n')
+
+
+
+#php
+os.system('toilet --filter border  PHP|lolcat')
+os.system('echo *************installing**************|lolcat')
+print('\n_______________________________________________________\n')
+os.system('pkg install php')
+print('\n_______________________________________________________\n')
+os.system('figlet DONE|lolcat')
+print('\n_______________________________________________________\n')
+
+
+#ssh
+os.system('toilet --filter border  SSH|lolcat')
+os.system('echo *************installing**************|lolcat')
+print('\n_______________________________________________________\n')
+os.system('pkg install openssh')
+print('\n_______________________________________________________\n')
+os.system('figlet DONE|lolcat')
+print('\n_______________________________________________________\n')
+
+#cmatrix
+os.system('toilet --filter border  CMATRIX|lolcat')
+os.system('echo ***************installing***************|lolcat')
+print('\n_______________________________________________________\n')
+os.system('pkg install cmatrix')
+print('\n_______________________________________________________\n')
+os.system('figlet DONE|lolcat')
+print('\n_______________________________________________________\n')
+
+
+#w3m
+os.system('toilet --filter border  w3m|lolcat')
+os.system('echo ***************installing***************|lolcat')
+print('\n_______________________________________________________\n')
+os.system('pkg install w3m -y')
+print('\n_______________________________________________________\n')
+os.system('figlet DONE|lolcat')
+print('\n_______________________________________________________\n')
+
+
+#curl
+os.system('toilet --filter border curl|lolcat')
+os.system('echo ****************installing**************|lolcat')
+print('\n_______________________________________________________\n')
+os.system('pkg install curl')
+print('\n_______________________________________________________\n')
+os.system('figlet DONE|lolcat')
+print('\n_______________________________________________________\n')
+
+#nano
+os.system('toilet --filter border NANO|lolcat')
+os.system('echo ****************installing**************|lolcat')
+print('\n_______________________________________________________\n')
+os.system('pkg install nano')
+print('\n_______________________________________________________\n')
+os.system('figlet DONE|lolcat')
+print('\n_______________________________________________________\n')
+
+
+
+os.system('apt update && apt upgrade -y')
+os.system('toilet -f mono12 -t --filter border:gay ALL SET')
+
+
+
+print('''\n
+
+     +-+-+-+-+-+-+   +-+-+-+-+-+-+-+-+-+-+-+-+
+•••• |A|u|t|h|o|r| : |s|q|l|a|s|s|k|i|c|k|e|r|••••
+     +-+-+-+-+-+-+   +-+-+-+-+-+-+-+-+-+-+-+-+    ''')
+
+
+print('\n________________github : https://github.com/sqlasskicker____________\n')
+
+
+
+os.system('bash')
