6c80823314e964c1be591442dbc63f4b747dd61c
sqlasskicker <63858650+sqlasskicker@users.noreply.github.com>
2020-09-30 14:40:54 +0530

    Add

---
+++ termux_pkg.sh
@@ -0,0 +1,229 @@
+​#​---colours---​
+red=​'​\033[1;91m​'​
+green=​'​\033[1;92m​'​
+yellow=​'​\033[1;93m​'​
+blue=​'​\033[1;94m​'​
+purple=​'​\033[1;95m​'​
+cyan=​'​\033[1;96m​'​
+white=​'​\033[1;97m​'​
+
+
+
+clear
+
+echo -e "​\033[1;96m        ​╔╦╗╔═╗╦═╗╔╦╗╦ ╦═╗ ╦   ╔═╗╦╔═╔═╗"
+echo -e "\033[1;96m​         ║ ║╣ ╠╦╝║║║║ ║╔╩╦╝───╠═╝╠╩╗║ ╦"
+echo -e "\033[1;96m​         ╩ ╚═╝╩╚═╩ ╩╚═╝╩ ╚═   ╩  ╩ ╩╚═╝"
+echo -e "\033[1;96m​         ===========××××××============="
+
+echo ""
+echo -e "\033[1;91m​ [•] AUTHOR : SQL ASSKICKER ™"
+echo ""
+echo -e "\033[1;91m​ [•] GITHUB :https://github.com/sqlasskicker" 
+echo ""
+echo -e "\033[1;95m​     ╔═╗╦ ╦╔╦╗╦ ╦╔═╗╔╗╔"
+echo -e "\033[1;95m​     ╠═╝╚╦╝ ║ ╠═╣║ ║║║║"
+echo -e "\033[1;95m​     ╩   ╩  ╩ ╩ ╩╚═╝╝╚╝"
+
+echo ""
+echo "[••••••• installing •••••••]"  
+echo ""
+pkg install python -y
+echo ""
+echo -e "\033[1;95m​[••••••• DONE •••••••]"
+
+echo ""
+echo ""
+
+
+echo -e "​\033[1;91m    ╔═╗╦ ╦╔╦╗╦ ╦╔═╗╔╗╔   ╔═╗"
+echo -e "​\033[1;91m    ╠═╝╚╦╝ ║ ╠═╣║ ║║║║───╔═╝"
+echo -e "​\033[1;91m    ╩   ╩  ╩ ╩ ╩╚═╝╝╚╝   ╚═╝"
+
+echo ""
+echo "[••••••• installing •••••••]"
+echo ""
+pkg install python2
+echo ""
+echo -e "\033[1;91m​[••••••• DONE •••••••]"
+
+echo ""
+echo ""
+
+echo -e "\033[1;93m​    ╔═╗╦╔═╗"
+echo -e "\033[1;93m​    ╠═╝║╠═╝"
+echo -e "\033[1;93m​    ╩  ╩╩  "
+echo ""
+echo "[••••• UPGRADING ••••••]"
+echo ""
+pip install --upgrade pip
+echo ""
+
+echo -e "\033[1;93m​[••••• DONE •••••]"
+echo ""
+echo ""
+echo -e "​\033[1;92m​    ╦═╗╦ ╦╔╗ ╦ ╦"
+echo -e "​\033[1;92m​    ╠╦╝║ ║╠╩╗╚╦╝"
+echo -e "​\033[1;92m​    ╩╚═╚═╝╚═╝ ╩ "
+echo ""
+echo "[••••• INSTALLING •••••]"
+
+echo ""
+pkg install ruby
+echo ""
+echo -e "\033[1;92m​[••••• DONE •••••]"
+
+echo ""
+echo ""
+
+
+echo -e " ​\033[1;94m​    ╦  ╔═╗╦  ╔═╗╔═╗╔╦╗"
+echo -e " ​\033[1;94m​    ║  ║ ║║  ║  ╠═╣ ║ "
+echo -e " ​\033[1;94m​    ╩═╝╚═╝╩═╝╚═╝╩ ╩ ╩ "
+echo ""
+echo "[••••• INSTALLING •••••]"
+echo ""
+gem install lolcat
+echo ""
+echo -e "\033[1;94m​[••••• DONE •••••]"
+
+echo ""
+echo ""
+
+
+echo -e "\033[1;91m​    ╔═╗╔═╗╦  ╔═╗╦═╗╔═╗╔╦╗╔═╗"
+echo -e "\033[1;91m​    ║  ║ ║║  ║ ║╠╦╝║ ║║║║╠═╣"
+echo -e "\033[1;91m​    ╚═╝╚═╝╩═╝╚═╝╩╚═╚═╝╩ ╩╩ ╩"
+echo ""
+echo "[••••• INSTALLING •••••]"
+echo ""
+pip install colorama
+echo ""
+echo -e "\033[1;91m​[••••• DONE •••••]"
+
+echo ""
+echo ""
+
+
+echo -e "\033[1;96m​    ╔═╗╔═╗╦ ╦"
+echo -e "\033[1;96m​    ╚═╗╚═╗╠═╣"
+echo -e "\033[1;96m​    ╚═╝╚═╝╩ ╩"
+echo ""
+echo "[••••• INSTALLING •••••]"
+echo ""
+pkg install openssh
+echo ""
+echo -e "\033[1;96m​[••••• DONE •••••]"
+
+echo ""
+echo ""
+
+echo -e "​\033[1;92m​    ╔═╗╦ ╦╔═╗"
+echo -e "​\033[1;92m​    ╠═╝╠═╣╠═╝"
+echo -e "​\033[1;92m​    ╩  ╩ ╩╩  "
+
+echo "[••••• INSTALLING •••••]"
+echo ""
+pkg install php
+echo ""
+echo -e "\033[1;92m​[••••• DONE •••••]"
+
+echo ""
+echo ""
+
+echo -e "\033[1;95m​    ╔═╗╔═╗╔═╗╔═╗╦ ╦╔═╗  ╔═╗╔═╗╦═╗╦  ╦╔═╗╦═╗"
+echo -e "\033[1;95m​    ╠═╣╠═╝╠═╣║  ╠═╣║╣   ╚═╗║╣ ╠╦╝╚╗╔╝║╣ ╠╦╝"
+echo -e "\033[1;95m​    ╩ ╩╩  ╩ ╩╚═╝╩ ╩╚═╝  ╚═╝╚═╝╩╚═ ╚╝ ╚═╝╩╚═"
+echo ""
+echo "[••••• INSTALLING •••••]"
+echo ""
+pkg install apache2 -y
+echo ""
+echo -e "\033[1;95m​[••••• DONE •••••]"
+
+echo ""
+echo ""
+
+
+echo -e " ​\033[1;94m​    ╔╗╔╔═╗╔╗╔╔═╗"
+echo -e " ​\033[1;94m​    ║║║╠═╣║║║║ ║"
+echo -e " ​\033[1;94m​    ╝╚╝╩ ╩╝╚╝╚═╝"
+echo ""
+echo "[••••• INSTALLING •••••]"
+echo ""
+pkg install nano
+echo ""
+echo -e "\033[1;94m​[••••• DONE •••••]"
+
+echo ""
+echo ""
+
+
+echo -e " ​\033[1;91m    ​╔═╗╦╔═╗╦  ╔═╗╔╦╗"
+echo -e " ​\033[1;91m    ╠╣ ║║ ╦║  ║╣  ║ "
+echo -e " ​\033[1;91m    ╚  ╩╚═╝╩═╝╚═╝ ╩ "
+
+echo ""
+echo "[••••• INSTALLING •••••]"
+echo ""
+apt install figlet
+
+echo ""
+echo -e "\033[1;91m​[••••• DONE •••••]"
+
+echo ""
+echo ""
+
+
+echo -e " ​\033[1;93m    ╔╦╗╔═╗╦╦  ╔═╗╔╦╗"
+echo -e " ​\033[1;93m     ║ ║ ║║║  ║╣  ║ "
+echo -e " ​\033[1;93m     ╩ ╚═╝╩╩═╝╚═╝ ╩ "
+
+echo ""
+echo "[••••• INSTALLING •••••]"
+echo ""
+apt install toilet
+echo ""
+echo -e "\033[1;93m​[••••• DONE •••••]"
+
+echo ""
+echo ""
+
+
+echo -e " ​\033[1;92m    ╔╦╗╔═╗╦  ╔═╗╔╦╗╦ ╦╔═╗╔╗╔"
+echo -e " ​\033[1;92m     ║ ║╣ ║  ║╣  ║ ╠═╣║ ║║║║"
+echo -e " ​\033[1;92m     ╩ ╚═╝╩═╝╚═╝ ╩ ╩ ╩╚═╝╝╚╝"
+echo ""
+echo "[••••• INSTALLING •••••]"
+echo ""
+pip install telethon
+echo ""
+echo -e "\033[1;92m​[••••• DONE •••••]"
+
+echo ""
+echo ""
+
+
+echo -e " ​\033[1;96m    ╦ ╦╔╗ ╔╦╗"
+echo -e " ​\033[1;96m    ║║║ ╩╗║║║"
+echo -e " ​\033[1;96m    ╚╩╝══╝╩ ╩"
+
+echo ""
+echo "[••••• INSTALLING •••••]"
+echo ""
+pkg install w3m -y
+echo ""
+echo -e "\033[1;96m​[••••• DONE •••••]"
+
+echo ""
+echo ""
+
+
+
+echo -e "​\033[1;97m    ╔═╗╦═╗╔═╗╔═╗╦═╗╔═╗╔╦╗  ╔═╗╦╔╗╔╦╔═╗╦ ╦╔═╗╔╦╗"
+echo -e "​\033[1;97m    ╠═╝╠╦╝║ ║║ ╦╠╦╝╠═╣║║║  ╠╣ ║║║║║╚═╗╠═╣║╣  ║║"
+echo -e "​\033[1;97m    ╩  ╩╚═╚═╝╚═╝╩╚═╩ ╩╩ ╩  ╚  ╩╝╚╝╩╚═╝╩ ╩╚═╝═╩╝"
+echo -e "​\033[1;97m    ==============××××asskicker××××============="
+
+echo ""
+echo ""
